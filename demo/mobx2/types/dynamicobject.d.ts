df3f00a064debe14d7fca836c95decc4cc7d868a
Taj Pereira <tajpereira@gmail.com>
2024-01-28 19:29:35 +1030

    fix: fix demo

---
+++ demo/mobx2/types/dynamicobject.d.ts
@@ -0,0 +1,5 @@
+import { IIsObservableObject, CreateObservableOptions } from "../internal"
+export declare function asDynamicObservableObject(
+    target: any,
+    options?: CreateObservableOptions
+): IIsObservableObject
